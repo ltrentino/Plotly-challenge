dbd1fef734a4ba79342bf4aeaf5099e95dc7ca20
Laura Trentino <laura@trentino.com.au>
2021-12-24 15:17:41 +0800

    cleaned some of order functions and reworking plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,67 +1,119 @@
 
+// INITIALIZE DASHBOARD
+function input() {     
+    // Test Subject ID n DROPDOWN menu
+    d3.json("samples.json").then((data)=>{
+        // console.log(data.names)
+        // feed Subject ID no into dropdown menu
+        data.names.forEach((name)=>{
+            d3.select("#selDataset")
+                .append("option")
+                .text(name)
+                .property("value");
 
-// Reading json file
+        });
+        // use first sample from list to feed initial plots
+        getPlots(data.names[0]);
+        demoInfo(data.names[0]);
+    });
+}
 
-d3.json("samples.json").then (data => {
-    console.log(data);  // names (153 subjects ids), metadata (153 rows with subjects ids info), samples (153 rows with nested otu's lists)
-    // get otu_ids
-    var otu_ids = data.samples[0].otu_ids;
-        console.log(otu_ids);
-    // Top 10 otu
-    var top_sample_values = data.samples[0].sample_values.slice(0,10).reverse();
-        console.log(`Top 10 otu's: ${top_sample_values}`);
-    var top_otu_labels = data.samples[0].otu_labels.slice(0,10).reverse();
-        console.log(`Top 10 otu's names: ${top_otu_labels}`);
-    // Bar plot
-        var trace = {
-            x: top_sample_values,
-            y: top_otu_labels,
-            type: "bar",
-            orientation: "h",
-        }
-        var data = [trace];
-        Plotly.newPlot("bar", data)
-})
+input();
+
+function selection(id) {
+    demoInfo(id);
+    getPlots(id);
+    
+};
 
 
-// demographic info box
-//function demographic_info(id) {
+
+// demographic info BOX
+function demoInfo(sample) {
     d3.json("samples.json").then((data)=> {
-        demographics =data.metadata.filter(x => x.id.toString() === "941")[0];
-        console.log(demographics);
+        // filter by id
+        var demographics = data.metadata.filter(x => x.id == sample)[0];
         // selecting html tag
         var demographic_tag = d3.select("#sample-metadata");
-        Object.entries(demographics).forEach((key) => {
-            demographic_tag.append("selDataset").text(key[0] + ": " + key[1] + "\n");
-
-        })
+        // clear data
+        demographic_tag.html("")
+            // appending data
+            Object.entries(demographics).forEach((key, value) => {
+            demographic_tag.append("h6").text(key + ": " + value);
+            });
     });
-//};
+};
 
 
 
+// CHARTS
+function getPlots(sample) {
+    // Reading json file
 
-d3.json("samples.json").then((data)=>{
-    console.log(data.names)
-    // feed Subject ID no into dropdown menu
-    data.names.forEach((name)=>{
-        d3.select("#selDataset").append("option").text(name).property("value");
+    d3.json("samples.json").then (data => {
+        console.log(data);  // names (153 subjects ids), metadata (153 rows with subjects ids info), samples (153 rows with nested otu's lists)
+        
+        var samples = data.samples;
+        console.log(samples);
+        
+        var resultArray = samples.filter(x=> x.id == sample);
 
-    })
-})
+        // first result in array
+        var result = resultArray[0];
+        console.log(result)
+        
+        // Variables to hold otu info
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = (result.sample_values.slice(0,11)).reverse();
+        console.log(otu_ids);
 
 
-// // Step 2: event listener
-// d3.select("#selDataset").on("change", ()=>{
-//     var selected = d3.select("#selDataset").property("value");
-//     // 
-//     console.log(selected);
-//     // Step 3: switch statements
-//     switch(selected){
-//       case "dataset1":
-//         trace = {
-//           x: []
-//         }
-//     }
-//   });
-  
+        // 
+        var top_otu_ids = (data.samples[0].otu_ids.slice(0,10)).reverse();
+        var otu_id = top_otu_ids.map(x => "OTU "+ x);
+        console.log(`OTU IDS: ${top_otu_ids}`)
+
+
+
+        // BAR PLOT
+        var trace = {
+            x: sample_values,
+            y: otu_id,
+            text: otu_labels,
+            type: "bar",
+            orientation: "h"
+          };
+            
+          var bar_data = [trace];
+          var config = {responsive: true};
+
+        Plotly.newPlot("bar", bar_data, config);
+
+        ///////////////////////////////////////////////////////
+
+        // BUBBLE CHART
+        var trace = {
+        x: result.otu_ids,
+        y: result.sample_values,
+        mode: 'markers',
+        marker: {
+            size: result.sample_values,
+            color: result.otu_ids,
+            colorscale: 'Earth',
+        },
+        text: result.otu_labels
+    };
+       
+        var bubble_data = [trace];
+        var config = {responsive: true};
+
+        Plotly.newPlot("bar", bubble_data, config);
+
+        
+
+
+    });
+};
+
+
